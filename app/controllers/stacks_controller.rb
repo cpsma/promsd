46e92ad7934fe745396f5025240b05f8bddb556b
Scott Heffner <sheffner@carlisle.k12.ma.us>
2023-09-30 17:40:24 -0400

    Return message when stack is successfully deleted

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -2,5 +2,6 @@ class StacksController < ApplicationController
   def destroy
     @stack = Stack.find_by(name: params[:name])
     @stack.destroy
+    render json: { message: "The stack '#{params[:name]}' and its scrapes have been deleted" }
   end
 end
--- test/controllers/stacks_controller_test.rb
+++ test/controllers/stacks_controller_test.rb
@@ -8,4 +8,13 @@ class StacksControllerTest < ActionDispatch::IntegrationTest
       delete stack_path(stack.name)
     end
   end
+
+  test "returns a message on successful deletion of stack" do
+    stack = stacks(:db)
+
+    delete stack_path(stack.name)
+
+    assert_equal "The stack 'db' and its scrapes have been deleted",
+      JSON.parse(response.body)["message"]
+  end
 end
