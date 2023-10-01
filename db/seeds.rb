848a1b6b58cfb56ddba84f3d3be09d6c6c0bc48c
Scott Heffner <sheffner@carlisle.k12.ma.us>
2023-09-30 20:41:55 -0400

    Add service seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Service.create(name: "node-exporter", port: 9100)
+
+Service.create(name: "prometheus", port: 9090)
+Service.create(name: "alertmanager", port: 9093)
