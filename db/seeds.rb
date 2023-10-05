05ea9bb738cf733bf5d6703585a444c71d366356
Scott Heffner <sheffner@carlisle.k12.ma.us>
2023-10-05 11:09:44 -0400

    Add mariadba and haproxy service

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Service.create(name: "node-exporter", port: 9100)
+Service.create(name: "mariadb", port: 9104)
+Service.create(name: "haproxy", port: 8404)
 
 Service.create(name: "prometheus", port: 9090)
 Service.create(name: "alertmanager", port: 9093)
+
+Service.create(name: "node-exporter", port: 9100)
